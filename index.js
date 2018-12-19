07c983df9a302c22fa9ff8b9db3a91f3ed2bf9ca
a97001@gmail.com <a97001@gmail.com>
2018-12-19 15:07:48 +0800

    tmp

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const config = require(process.cwd()+'/config.json');
+const config = require(`${process.cwd()}/config.json`);
 const csv = require('fast-csv');
 const fs = require('fs');
 const path = require('path');
-const sql = require("mssql/msnodesqlv8");
+const sql = require('mssql');
+const SqlResultProcessor = require('./lib/SqlResultProcessors');
 
 (async () => {
     const now = Date.now();
     const pool = new sql.ConnectionPool({
-        // server: "COMON-PC",
-        // database: "test",
+        server: config.connection.server,
+        database: config.connection.database,
+        user: config.connection.user,
+        password: config.connection.password
         // options: {
         //     trustedConnection: 'yes'
         // }
-        connectionString: config.connectionString
+        // connectionString: 'Driver={SQL Server Native Client 11.0};Server={COMON-PC};Database={test};Trusted_Connection={yes};'
     });
 
     await pool.connect();
 
     const request = new sql.Request(pool);
-    request.input('timestamp', sql.TYPES.DateTime, now);
+    // request.input('timestamp', sql.TYPES.DateTime, now);
     const result = await request.query(config.query);
 
-    console.dir(result);
+    // console.dir(result);
 
     if (result && result.recordsets) {
         let pendingCSVCount = result.recordsets.length;
@@ -30,20 +33,15 @@ const sql = require("mssql/msnodesqlv8");
             const csvName = `${config.csvName}_${recordsetIdx}_${new Date().valueOf()}.csv`;
             const csvStream = csv.createWriteStream({ headers: true });
             const writableStream = fs.createWriteStream(path.join(config.csvExportDir, csvName));
-            writableStream.on("finish", function () {
-                console.log("DONE!");
+            writableStream.on('finish', () => { // eslint-disable-line no-loop-func
+                console.log('DONE!');
                 pendingCSVCount--;
                 if (pendingCSVCount === 0) {
                     process.exit();
                 }
             });
             csvStream.pipe(writableStream);
-
-            for (const record of recordset) {
-                record.timestamp = new Date(record.timestamp).toISOString()
-                console.log(record);
-                csvStream.write(record);
-            }
+            SqlResultProcessor[config.sqlResultProcessor](csvStream, recordset, now);
             csvStream.end();
             recordsetIdx++;
         }
